cb01d722f41c02530abe7e1d47431a5e482aa238
Kevin Vuong <kev2003@med.cornell.edu>
2018-03-16 12:26:58 -0400

    Added upload portfolio lambda python script

---
+++ upload-portfolio-lambda.py
@@ -0,0 +1,21 @@
+import boto3
+import io
+import zipfile
+import mimetypes
+
+s3 = boto3.resource('s3')
+
+portfolio_bucket = s3.Bucket('portfolio.junosummer.com')
+
+
+build_bucket = s3.Bucket('portfoliobuild.junosummer.com')
+portfolio_zip = io.BytesIO()
+
+build_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
+
+with zipfile.ZipFile(portfolio_zip) as myzip:
+    for name in myzip.namelist():
+        obj = myzip.open(name)
+        portfolio_bucket.upload_fileobj(obj,name,
+            ExtraArgs={'ContentType': mimetypes.guess_type(name)[0]})
+        portfolio_bucket.Object(name).Acl().put(ACL='public-read')
