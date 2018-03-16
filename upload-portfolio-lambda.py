70693617abb64ae85f699857970e1b69be2725ad
Kevin Vuong <kev2003@med.cornell.edu>
2018-03-16 12:55:44 -0400

    Added upload portfolio lambda python script v2 with lambda function in mind

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -3,19 +3,23 @@ import io
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3')
 
-portfolio_bucket = s3.Bucket('portfolio.junosummer.com')
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3')
+    
+    portfolio_bucket = s3.Bucket('portfolio.junosummer.com')
+    
+    
+    build_bucket = s3.Bucket('portfoliobuild.junosummer.com')
+    portfolio_zip = io.BytesIO()
+    
+    build_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
+    
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for name in myzip.namelist():
+            obj = myzip.open(name)
+            portfolio_bucket.upload_fileobj(obj,name,
+                ExtraArgs={'ContentType': mimetypes.guess_type(name)[0]})
+            portfolio_bucket.Object(name).Acl().put(ACL='public-read')
 
-
-build_bucket = s3.Bucket('portfoliobuild.junosummer.com')
-portfolio_zip = io.BytesIO()
-
-build_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
-
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for name in myzip.namelist():
-        obj = myzip.open(name)
-        portfolio_bucket.upload_fileobj(obj,name,
-            ExtraArgs={'ContentType': mimetypes.guess_type(name)[0]})
-        portfolio_bucket.Object(name).Acl().put(ACL='public-read')
+    return "hello from Build portfolio"
