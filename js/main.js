a67428ab23bfe8b3919c8fce712b5b69dd1b80a8
Kevin Vuong <kevin@med.cornell.edu>
2018-03-19 16:44:50 -0400

    Move static markup for work examles to react

--- js/main.js
+++ js/main.js
@@ -1,5 +1,32 @@
-import 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work';
 
-console.log("Loaded react-dom");
+const myWork = [
+    {
+        'title': "Work Example",
+        'image': {
+            'desc': "example screenshot of a project involving code",
+            'src': "images/example1.png",
+            'comment': ""
+        }
+    },
+    {
+        'title': "Lambda Boilerplate",
+        'image': {
+            'desc': "Serverless Portfolio",
+            'src': "images/lambdadelete.png",
+            'comment': ""
+        }
+    },
+    {
+        'title': "Abot Snapshot",
+        'image': {
+            'desc': "Abot Snapshot",
+            'src': "images/abotsnapshot.png",
+            'comment': ""
+        }
+    }
+]
 
-console.log("Webpack Wworks~");
+ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
